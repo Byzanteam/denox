df655b695bfafd5f5727d9b442cfe48a2df502f2
Phil Chen <06fahchen@gmail.com>
2023-02-27 21:23:46 +0800

    ci: init

--- lib/denox.ex
+++ lib/denox.ex
@@ -2,17 +2,4 @@ defmodule Denox do
   @moduledoc """
   Documentation for `Denox`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Denox.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/denox_test.exs
+++ test/denox_test.exs
@@ -1,8 +1,4 @@
 defmodule DenoxTest do
   use ExUnit.Case
   doctest Denox
-
-  test "greets the world" do
-    assert Denox.hello() == :world
-  end
 end
