c4ecf0f6b213c2a1817a4b87d09676cd5a4f2f83
Phil Chen <06fahchen@gmail.com>
2023-02-27 21:06:39 +0800

    first commit

---
+++ lib/denox.ex
@@ -0,0 +1,18 @@
+defmodule Denox do
+  @moduledoc """
+  Documentation for `Denox`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Denox.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
